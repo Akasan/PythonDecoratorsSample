961f2bf282ba2828e66213c921e2245001e449bd
Akasan <daisuke1024akagawa@gmail.com>
2021-06-23 13:08:26 +0900

    Update timer

--- DADecorator/_Timer.py
+++ DADecorator/_Timer.py
@@ -1,20 +1,48 @@
 import time
 
 
-def timer(*vargs, **vkwargs):
-    iteration = vkwargs.get("iteration", 1)
+def timer(iteration: int = 1, is_max: bool = False, is_min: bool = False):
+    """
+    Keyword Arguments:
+    ------------------
+        iteration {int} -- the number of iterations you want to execute the function
+                           (default: 1)
+        is_max {bool} -- if you want to print the maximum time, set this as True
+                         (default: False)
+        is_min {bool} -- if you want to print the minimum time, set this as True
+                         (default: False)
+
+    Examples:
+    ---------
+        >>> @timer()
+        ... def hoge():
+        ...     ...
+    """
 
     def _timer(func):
         def wrapper(*args, **kwargs):
-            st = time.time()
+            t = 0
+            _min = -1
+            _max = -1
             
             for _ in range(iteration):
+                st = time.time()
                 func(*args, **kwargs)
+                et = time.time()
+                dt = et - st
+                t += dt
 
-            end = time.time()
+                if is_min and (_min == -1 or _min < dt):
+                    _min = dt
+                if is_max and (_max == -1 or _max > dt):
+                    _max = dt
 
             print(f"Execute {func.__name__} for {iteration} times.")
-            print(f"It took {(end - st) / iteration} second as average.")
+            print(f"It took {dt / iteration} second as average.")
+            if is_min:
+                print(f"Minimum time is {_min}")
+            if is_max:
+                print(f"Maximum time is {_max}")
 
         return wrapper
 
